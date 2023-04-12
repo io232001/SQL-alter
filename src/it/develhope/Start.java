edce25087e9651f889fe5a4bc77d9b1712caa1c2
iosbd <io232001@gmail.com>
2023-04-12 12:05:42 +0200

    Initial commit

---
+++ src/it/develhope/Start.java
@@ -0,0 +1,38 @@
+package it.develhope;
+import java.sql.*;
+public class Start {
+    public static void main(String[] args)
+    {
+        Connection collection=null;
+        try {
+            String url       = "jdbc:mysql://localhost:3306/newdb";
+            String user      = "developer";
+            String password  = "passwordhere";
+            collection = DriverManager.getConnection(url, user, password);
+            Statement statement = collection.createStatement();
+            String query = ("ALTER TABLE student ADD COLUMN country VARCHAR(30)");
+            statement.executeUpdate(query);
+            statement.executeUpdate("UPDATE student SET country = 'France' where student_id = 1;");
+            statement.executeUpdate("UPDATE student SET country = 'Italy' where student_id = 2;");
+            statement.executeUpdate("UPDATE student SET country = 'Germany' where student_id = 3;");
+            statement.executeUpdate("UPDATE student SET country = 'Spain' where student_id = 10;");
+        }
+        catch(SQLException e)
+        {
+            System.out.println("error");
+            System.out.println(e.getMessage());
+        }
+        finally
+        {
+            try
+            {
+                if(collection != null)
+                    collection.close();
+            }
+            catch(SQLException ex)
+            {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+}
