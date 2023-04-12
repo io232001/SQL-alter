e1f65725207992ba0f9f249f5c9dcb194b669e7c
iosbd <io232001@gmail.com>
2023-04-12 12:06:44 +0200

    Commmit

--- src/it/develhope/Start.java
+++ src/it/develhope/Start.java
@@ -12,10 +12,10 @@ public class Start {
             Statement statement = collection.createStatement();
             String query = ("ALTER TABLE student ADD COLUMN country VARCHAR(30)");
             statement.executeUpdate(query);
-            statement.executeUpdate("UPDATE student SET country = 'France' where student_id = 1;");
+            statement.executeUpdate("UPDATE student SET country = 'Italy' where student_id = 1;");
             statement.executeUpdate("UPDATE student SET country = 'Italy' where student_id = 2;");
             statement.executeUpdate("UPDATE student SET country = 'Germany' where student_id = 3;");
-            statement.executeUpdate("UPDATE student SET country = 'Spain' where student_id = 10;");
+            statement.executeUpdate("UPDATE student SET country = 'Germany' where student_id = 10;");
         }
         catch(SQLException e)
         {
